ce7b50cebd1e241bff51ff27b0405632bf34c621
Half_nothing <Half_nothing@163.com>
2024-12-04 05:18:47 +0800

    chore: 配置Gradle构建流程

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,29 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.text.SimpleDateFormat
+import java.util.*
+
 plugins {
-    kotlin("jvm") version "1.9.23"
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.git.hooks)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.shadow)
 }
 
-group = "cn.half.nothing"
-version = "1.0-SNAPSHOT"
+val props = properties
+
+val srcVersion: String by project
+val mavenGroup: String by project
+val targetJavaVersion = 17
+val javaVersion = JavaVersion.VERSION_17
+
+version = "$srcVersion${getVersionMetadata()}"
+group = mavenGroup
 
 repositories {
+    maven("https://maven.aliyun.com/repository/public/")
+    maven("https://maven.aliyun.com/repository/gradle-plugin/")
+    mavenLocal()
     mavenCentral()
 }
 
@@ -13,10 +31,72 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+
+base {
+    archivesName.set(rootProject.name)
 }
 
-kotlin {
-    jvmToolchain(17)
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    withSourcesJar()
+}
+
+tasks {
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(targetJavaVersion)
+    }
+
+    withType<KotlinCompile>().configureEach {
+        compilerOptions.jvmTarget.set(JvmTarget.fromTarget(targetJavaVersion.toString()))
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    jar {
+        from("LICENSE")
+        manifest {
+            attributes(
+                "Build-By" to System.getProperty("user.name"),
+                "Build-TimeStamp" to SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(Date()),
+                "Build-Version" to version,
+                "Created-By" to "Gradle ${gradle.gradleVersion}",
+                "Build-Jdk" to "${System.getProperty("java.version")} " +
+                        "(${System.getProperty("java.vendor")} ${System.getProperty("java.vm.version")})",
+                "Build-OS" to "${System.getProperty("os.name")} " +
+                        "${System.getProperty("os.arch")} ${System.getProperty("os.version")}"
+            )
+        }
+    }
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    autoCorrect = true
+    config.setFrom(rootProject.files("detekt.yml"))
+}
+
+gitHooks {
+    setHooks(
+        mapOf("pre-commit" to "detekt")
+    )
+}
+
+fun getVersionMetadata(): String {
+    val buildId = System.getenv("GITHUB_RUN_NUMBER")
+    val workflow = System.getenv("GITHUB_WORKFLOW")
+
+    if (workflow == "Release") {
+        return ""
+    }
+
+    if (buildId != null) {
+        return "+build.$buildId"
+    }
+
+    return "+nightly"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,13 @@
+rootProject.name = "payroll-manager"
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "payroll-manager"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
+    }
+}
